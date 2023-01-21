8b97b6006d9c6ea65d99de14c454ba9be4188f9a
willbraken <brandon@willbraken.com>
2023-01-21 11:33:02 -0600

    add all standard functionality

--- main.rb
+++ main.rb
@@ -10,15 +10,16 @@ class LinkedList
   # adds a new node containing #value to the end of the list
   def append(value)
     new_node = Node.new(value)
-    if @tail.nil?
-      @tail = new_node
+    if @head.nil?
+      @head = new_node
+    elsif @tail.nil?
+      @head.next_node = new_node
     else
       @tail.next_node = new_node
-      @tail = new_node
     end
-    @head = new_node if @head.nil?
+    @tail = new_node
   end
-  
+
   # adds a new node containing #value to the start of the list
   def prepend(value)
     new_node = Node.new(value, @head)
@@ -41,16 +42,6 @@ class LinkedList
     size
   end
 
-  # returns the first node in the list
-  def head
-    @head
-  end
-
-  # returns the last node in the list
-  def tail
-    @tail
-  end
-
   # returns the node at the given index
   def at(index)
     current_node = @head
@@ -69,29 +60,67 @@ class LinkedList
     current_node = @head
     if current_node.next_node == @tail
       @tail = current_node
+      @tail.next_node = nil
     elsif current_node == @head && current_node.next_node.nil?
       @head = nil
+      current_node.value = nil
     else
       until current_node.next_node == @tail
         current_node = current_node.next_node
       end
       current_node.next_node = nil
+      @tail = current_node
     end
   end
 
-  def contains?(value)  # returns true if the passed in value is in the list and otherwise returns false
+  # returns true if the passed in value is in the list and otherwise returns false
+  def contains?(value)
+    current_node = @head
+    until current_node.next_node.nil?
+      if current_node.value == value
+        return true
+      else
+        current_node = current_node.next_node
+        false
+      end
+    end
   end
 
-  def find(value) # returns the index of the node containing value, or nil if not found
+  # returns the index of the node containing value, or nil if not found
+  def find(value)
+    current_node = @head
+    index = 0
+    until current_node.value == value
+      if current_node.next_node.nil? && current_node.value != value
+        nil
+      else
+        current_node = current_node.next_node
+        index += 1
+      end
+    end
+    index
   end
 
-  def to_s  # represents LinkedList objects as strings, so that they can be printed and viewed in the console
+  # represents LinkedList objects as strings, so that they can be printed and viewed in the console
+  def to_s
+    string = ''
+    current_node = @head
+    until current_node.nil?
+      string += "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
+    end
+    string += ' nil'
+    string
   end
 
-  def insert_at(value, index) # inserts new node with provided value at the given index
+  # inserts new node with provided value at the given index
+  def insert_at(value, index) 
+    current_node = @head
+    previous_node = nil
   end
 
-  def remove_at(index)  # removes the node at the given index
+  # removes the node at the given index
+  def remove_at(index)
   end
 end
 
@@ -111,5 +140,4 @@ list.append('soulja boy')
 puts list.head.value
 puts list.tail.value
 puts list.size
-list.pop
-puts list.size
+puts list.to_s
